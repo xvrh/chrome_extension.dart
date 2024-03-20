d0cef44f9bc2e9e6527328fed57aae4284bafc78
Xavier Hainaux <xavier.hainaux@gmail.com>
2024-03-20 09:56:11 +0100

    run prepare_submit.dart

--- extension_examples/flutter2/lib/main.dart
+++ extension_examples/flutter2/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:chrome_extension/tabs.dart';
 import 'package:flutter/material.dart' hide Tab;
 import 'package:web/web.dart' as web;
-
 import 'qr_view.dart';
 
 void main() => runApp(const MyApp());
