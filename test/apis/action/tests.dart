aaa621e5bc21b4d01d6daf731d81da77b981d39c
Xavier Hainaux <xavier.hainaux@gmail.com>
2024-05-13 15:00:18 +0200

    +

--- test/apis/action/tests.dart
+++ test/apis/action/tests.dart
@@ -164,6 +164,7 @@ void _tests(TestContext context) {
     var getPopupParams = TabDetails(tabId: tab.id);
 
     await chrome.action.setPopup(popupParams);
+    await _addSmallDelay();
     var actual = await chrome.action.getPopup(getPopupParams);
     check(actual).endsWith(popupFile); // adds extension prefix
     await chrome.action.setPopup(clearPopupParams);
